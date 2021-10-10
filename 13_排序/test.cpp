3e88599218668315f8192d19d5db54a1a144716a
Sugar-27 <45682h@gmail.com>
2021-10-10 18:27:29 +0800

    update WeekRate

--- 13_排序/test.cpp
+++ 13_排序/test.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 void bubSort(vector<int>& nums);
-int main() {
-    vector<int> nums = {4, 1, 5, 3, 9};
-    for (auto num : nums) {
-        cout << num << " ";
-    }
-    cout << endl;
-    bubSort(nums);
-    for (auto num : nums) {
-        cout << num << " ";
-    }
-    cout << endl;
-}
+// int main() {
+//     vector<int> nums = {4, 1, 5, 3, 9};
+//     for (auto num : nums) {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+//     bubSort(nums);
+//     for (auto num : nums) {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+// }
 
 void bubSort(vector<int>& nums) {
     int n = nums.size();
@@ -26,4 +26,17 @@ void bubSort(vector<int>& nums) {
             }
         }
     }
+}
+
+int main() {
+    vector<int> a = {6,6,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};
+    vector<int> b = {4,5,6,2,3,6,5,4,6,4,5,1,6,3,1,4,5,5,3,2,3,5,3,2,1,5,4,3,5,1,5};
+    vector<int> c = {4,4,4,4,4,4,5,5,4,4,4,5,4,4,4,4,4,4,4,4,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5};
+    int sums = 0;
+    for (int s : a) sums +=s;
+    cout << sums << endl;
+    cout << a.size() << endl;
+    sums = 0;
+    for (int s : c) sums +=s;
+    cout << sums << endl;
 }
