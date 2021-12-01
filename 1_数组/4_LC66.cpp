70844149df040ce8a99d87ca9689cb96aeda12ed
Sugar <45682h@gmail.com>
2021-12-01 11:14:30 +0800

    优化LC66代码

--- 1_数组/4_LC66.cpp
+++ 1_数组/4_LC66.cpp
@@ -7,14 +7,13 @@ class Solution {
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
         for (int i = n - 1; i >= 0; --i) {
-            digits[i]++;
-            if (digits[i] < 10)
+            digits[i] = (digits[i] + 1) % 10;
+            if (digits[i] != 0)
                 break;
-            else
-                digits[i] = 0;
         }
-        if (digits[0] == 0)
+        if (digits[0] == 0) {
             digits.insert(digits.begin(), 1);
+        }
         return digits;
     }
 };
