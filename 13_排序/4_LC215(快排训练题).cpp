93ead63c6b2e2a5922a942b2c20959a5d17ae2bc
Sugar <45682h@gmail.com>
2022-01-02 01:30:53 +0800

    修改LC215

--- 13_排序/4_LC215(快排训练题).cpp
+++ 13_排序/4_LC215(快排训练题).cpp
@@ -1,33 +1,38 @@
 //本质也是排序的模板题
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+        this->k = k;
         // 第K大，等于（排序后下标从0开始）第N-K小
-        return quickSort(nums, nums.size() - k, 0, nums.size() - 1);
+        quickSort(nums, 0, nums.size() - 1);
+        return nums[nums.size() - k];
     }
 
 private:
-    int quickSort(vector<int>& nums, int k, int l, int r) {
-        if (l == r) return nums[l];
-        int pivot = partition(nums, l, r);
-        if (pivot >= k) return quickSort(nums, k, l, pivot);
-        else return quickSort(nums, k, pivot + 1, r);
-    }
-
-    int partition(vector<int>& a, int l, int r) {
-        int pivot = l + rand() % (r - l + 1);
-        int pivotVal = a[pivot];
-
-        while (l <= r) {
-            while (a[l] < pivotVal) l++;
-            while (a[r] > pivotVal) r--;
-            if (l <= r) {
-                int temp = a[l]; a[l] = a[r]; a[r] = temp;
-                l++; r--;
-            }
+    int k;
+    void quickSort(vector<int>& nums, int l, int r) {
+        if (l >= r) return;
+        int pivot = partition(nums, l, r);
+        if (pivot >= nums.size() - k) {
+            quickSort(nums, l, pivot);
+        } else {
+            quickSort(nums, pivot + 1, r);
         }
-
+    }
+    int partition(vector<int>& nums, int l, int r) {
+        int pivot = l + rand() % (r - l + 1);
+        int pivotVal = nums[pivot];
+        while (l <= r) {
+            while (nums[l] < pivotVal)  ++l;
+            while (nums[r] > pivotVal)  --r;
+            if (r == l) break;
+            if (l < r) 
+                swap(nums[l++], nums[r--]);
+        }
         return r;
     }
 };
