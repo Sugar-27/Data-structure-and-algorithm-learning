136b5dce4c27ad39f813391ff930922865d423b6
Sugar <45682h@gmail.com>
2022-03-05 00:36:05 +0800

    增加解法

--- 8_递归、分治/7_LC104.cpp
+++ 8_递归、分治/7_LC104.cpp
@@ -1,38 +1,39 @@
 // 递归找出二叉树的最大深度
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// 记录局部变量depth，最容易理解的做法
 class Solution {
-public:
+   public:
     int maxDepth(TreeNode* root) {
-        if (root == nullptr) return 0;
+        dfs(root, 0);
+        return ans;
+    }
+
+   private:
+    void dfs(TreeNode* root, int depth) {
+        if (!root) {
+            ans = max(ans, depth);
+            return;
+        }
+        dfs(root->left, depth + 1);
+        dfs(root->right, depth + 1);
+    }
+    int ans = 0;
+};
+
+// 极简解法
+class Solution {
+   public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
         return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
 
 //第二种递归方式，记录全局变量depth
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
-public:
+   public:
     int maxDepth(TreeNode* root) {
         depth = 1;
         ans = 0;
@@ -40,9 +41,10 @@ public:
         return ans;
     }
 
-private:
+   private:
     void calc(TreeNode* root) {
-        if (root == nullptr)    return;
+        if (root == nullptr)
+            return;
         ans = max(ans, depth);
         depth++;
         calc(root->left);
