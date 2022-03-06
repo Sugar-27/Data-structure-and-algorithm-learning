6aef315b9db3104e29c25752c35db2593cb80cf3
Sugar <45682h@gmail.com>
2022-03-06 13:15:49 +0800

    修改README

--- 10_dfs_bfs/10_LC301.cpp
+++ 10_dfs_bfs/10_LC301.cpp
@@ -1,20 +1,27 @@
+// 看到这种要给出方案的题，首先要想办法确定目标
+// 最小括号，可以想到括号数目是有标准答案的
+// 因此目标就变为了如何删可以让括号数量变成我们的目标
+
 class Solution {
-public:
+   public:
     vector<string> removeInvalidParentheses(string s) {
         this->s = s;
         n = s.length();
-        int l = 0, r = 0; // l表示要删除的左括号，r表示要删除的右括号
-        int c1 = 0, c2 = 0; // c1表示总的左括号，c2表示总的右括号
+        int l = 0, r = 0;  // l表示要删除的左括号，r表示要删除的右括号
+        int c1 = 0, c2 = 0;  // c1表示总的左括号，c2表示总的右括号
         for (char ch : s) {
-            if (ch == '(')  ++l, ++c1;
+            if (ch == '(')
+                ++l, ++c1;
             else if (ch == ')') {
-                if (l != 0) --l;
-                else ++r;
+                if (l != 0)
+                    --l;
+                else
+                    ++r;
                 ++c2;
             }
         }
         len = n - l - r;
-        score_max = min(c1, c2); // 计算最大的分数，一对完整的括号积一分
+        score_max = min(c1, c2);  // 计算最大的分数，一对完整的括号积一分
         dfs(0, "", l, r, 0);
         vector<string> ans;
         for (string k : removes) {
@@ -22,23 +29,26 @@ public:
         }
         return ans;
     }
-private:
+
+   private:
     void dfs(int u, string cur, int l, int r, int score) {
         // 要删除的小于零了说明删多了
         // 对于一个合法的字符串，score永远要大于等于0，即左括号的数量大于等于右括号，同时不能超过最大分数限制，超过了则没有相应的右括号能减到0
-        if (l < 0 || r < 0 || score < 0 || score > score_max) return;
+        if (l < 0 || r < 0 || score < 0 || score > score_max)
+            return;
         // 要删的括号删完了
         if (l == 0 && r == 0) {
-            if (cur.length() == len)    removes.insert(cur);
+            if (cur.length() == len)
+                removes.insert(cur);
         }
         // 遍历到了最后一个字符，且要删的没删完，就没必要继续了
-        if (u == n) return;
+        if (u == n)
+            return;
         char c = s[u];
         if (c == '(') {
             dfs(u + 1, cur + "(", l, r, score + 1);
             dfs(u + 1, cur, l - 1, r, score);
-        }
-        else if (c == ')') {
+        } else if (c == ')') {
             dfs(u + 1, cur + ")", l, r, score - 1);
             dfs(u + 1, cur, l, r - 1, score);
         } else {
--- 10_dfs_bfs/README.md
+++ 10_dfs_bfs/README.md
@@ -13,3 +13,41 @@
 #### 在状态空间为一般的图时（即需要判重），DFS和BFS差不多
 
 * 按个人喜好来即可
+
+## 关于地图类的搜索题
+
+### 基本套路：
+
+1. 先找给定地图范围，也即确定`int m, n`，`m = board.size()`,`n = board[0].size()`，其中`m`是行数而`n`是列数
+
+2. 然后确定方向，也即
+   `const int dx[4] = {-1, 0, 1, 0};`
+   `const int dy[4] = {0, -1, 0, 1};`
+   也就是按照[上，左，下，右]的顺序来，如果有方向要求，向左转就是`(dir + 1) % 4`，向右转就是`(dir - 1 + 4) % 4`
+
+3. 写子程序，按照规则完成任务
+
+   ```c++
+   void dfs(vector<vector<char>>& board, int x, int y, char ch, bool record) {
+           visit[x][y] = true;
+     			// 要完成的任务
+           if (record) {
+               rec.insert({x, y});
+           }
+     			// 要完成的任务
+           board[x][y] = ch;
+           for (int i = 0; i < 4; ++i) {
+             	// 遍历地图
+               int nx = x + dx[i];
+               int ny = y + dy[i];
+               if (nx < 0 || ny < 0 || nx >= m || ny >= n || visit[nx][ny]) {
+                   continue;
+               }
+             	// 达到条件可以继续遍历
+               if (board[nx][ny] == 'O')
+                   dfs(board, nx, ny, ch, record);
+           }
+       }
+   ```
+
+   
