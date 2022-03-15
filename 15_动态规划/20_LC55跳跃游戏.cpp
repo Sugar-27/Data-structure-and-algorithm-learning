0548e2e655d7bf4933d0d585aa0a02408b959e21
Sugar <45682h@gmail.com>
2022-03-15 22:02:21 +0800

    去除错误

--- 15_动态规划/20_LC55跳跃游戏.cpp
+++ 15_动态规划/20_LC55跳跃游戏.cpp
@@ -1,18 +1,3 @@
-// 动态规划，会超时
-class Solution {
-   public:
-    int numSquares(int n) {
-        vector<int> dp(n + 1, 1e9);
-        dp[0] = 0;
-        for (int i = 1; i * i <= n; ++i) {
-            int x = i * i;
-            for (int j = x; j <= n; ++j) {
-                dp[j] = min(dp[j], dp[j - x] + 1);
-            }
-        }
-        return dp[n];
-    }
-};
 
 // 贪心
 class Solution {
