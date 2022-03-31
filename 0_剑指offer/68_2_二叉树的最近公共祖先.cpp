ce24d996d099333eb2d7f49c09e8cfaf832e605e
Sugar <45682h@gmail.com>
2022-03-31 10:26:08 +0800

    更新题目

--- 0_剑指offer/68_2_二叉树的最近公共祖先.cpp
+++ 0_剑指offer/68_2_二叉树的最近公共祖先.cpp
@@ -51,3 +51,17 @@ public:
         return l == nullptr ? r : (r == nullptr ? l : root);
     }
 };
+
+// 逻辑更清晰的版本
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        // 先把两边都有的情况判出去
+        if (left && right) return root;
+        // 再考虑在一边的情况
+        return left ? left : right;
+    }
+};
