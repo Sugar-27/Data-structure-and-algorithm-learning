7ad951f5b07873095335c99b2496549ea8ff0d4f
Sugar <45682h@gmail.com>
2022-04-01 01:31:31 +0800

    更新题解

--- 15_动态规划/31_LC5最长回文子串.cpp
+++ 15_动态规划/31_LC5最长回文子串.cpp
@@ -1,20 +1,20 @@
 class Solution {
-   public:
+public:
     string longestPalindrome(string s) {
         int n = s.length();
         vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int maxLen = 0, start = 0;
+        int maxlen = 0, start = 0;
         for (int j = 0; j < n; ++j) {
-            for (int i = 0; i <= n; ++i) {
+            for (int i = 0; i <= j; ++i) {
                 if (s[i] == s[j] && (j - i < 3 || dp[i + 1][j - 1])) {
                     dp[i][j] = true;
-                    if (j - i + 1 > maxLen) {
-                        maxLen = j - i + 1;
+                    if (j - i + 1 > maxlen) {
                         start = i;
+                        maxlen = j - i + 1;
                     }
                 }
             }
         }
-        return s.substr(start, maxLen);
+        return s.substr(start, maxlen);
     }
 };
