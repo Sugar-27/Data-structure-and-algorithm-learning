ea0a0bc6aa2ab2902ae1ca6da886e433749e6729
Sugar <45682h@gmail.com>
2023-09-24 21:29:55 +0800

    [feat] 优化写法

--- 15_动态规划/31_LC5最长回文子串.cpp
+++ 15_动态规划/31_LC5最长回文子串.cpp
@@ -1,56 +1,59 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n = s.length();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int maxlen = 0, start = 0;
-        for (int j = 0; j < n; ++j) {
-            for (int i = 0; i <= j; ++i) {
-                if (s[i] == s[j] && (j - i < 3 || dp[i + 1][j - 1])) {
-                    dp[i][j] = true;
-                    if (j - i + 1 > maxlen) {
-                        start = i;
-                        maxlen = j - i + 1;
-                    }
-                }
-            }
+  string longestPalindrome(string s) {
+    int n = s.length();
+    int ans_begin = 0, ans_len = 1;
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    for (int i = 0; i < n; ++i)
+      dp[i][i] = true;
+    for (int i = n - 1; i >= 0; --i) {
+      for (int j = i + 1; j < n; ++j) {
+        int len = j - i + 1;
+        if (s[i] == s[j]) {
+          dp[i][j] = len <= 3 || dp[i + 1][j - 1];
+          if (dp[i][j] && len > ans_len) {
+            ans_begin = i;
+            ans_len = len;
+          }
         }
-        return s.substr(start, maxlen);
+      }
     }
+    return s.substr(ans_begin, ans_len);
+  }
 };
 
 // 中心拓展法，更快
 class Solution {
-  public:
-    string longestPalindrome(string s) {
-        int n = s.length();
-        int ansIdx = -1;
-        int ansLen = INT_MIN;
-        s.insert(s.begin(), ' ');
-        for (int center = 1; center <= n; ++center) {
-            int l = center - 1, r = center + 1;
-            int len = 1;
-            while (l > 0 && r <= n && s[l] == s[r]) {
-                --l, ++r;
-                len += 2;
-            }
-            if (len > ansLen) {
-                ansIdx = l + 1;
-                ansLen = len;
-            }
-        }
-        for (int center = 1; center < n; ++center) {
-            int l = center, r = center + 1;
-            int len = 0;
-            while (l > 0 && r <= n && s[l] == s[r]) {
-                --l, ++r;
-                len += 2;
-            }
-            if (len > ansLen) {
-                ansIdx = l + 1;
-                ansLen = len;
-            }
-        }
-        return s.substr(ansIdx, ansLen);
+public:
+  string longestPalindrome(string s) {
+    int n = s.length();
+    int ansIdx = -1;
+    int ansLen = INT_MIN;
+    s.insert(s.begin(), ' ');
+    for (int center = 1; center <= n; ++center) {
+      int l = center - 1, r = center + 1;
+      int len = 1;
+      while (l > 0 && r <= n && s[l] == s[r]) {
+        --l, ++r;
+        len += 2;
+      }
+      if (len > ansLen) {
+        ansIdx = l + 1;
+        ansLen = len;
+      }
     }
+    for (int center = 1; center < n; ++center) {
+      int l = center, r = center + 1;
+      int len = 0;
+      while (l > 0 && r <= n && s[l] == s[r]) {
+        --l, ++r;
+        len += 2;
+      }
+      if (len > ansLen) {
+        ansIdx = l + 1;
+        ansLen = len;
+      }
+    }
+    return s.substr(ansIdx, ansLen);
+  }
 };
