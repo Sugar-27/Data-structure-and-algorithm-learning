b02596a67d1814dd9725d008a4e2bd2740123793
Sugar <45682h@gmail.com>
2023-09-24 21:10:51 +0800

    [feat] 增加解法

--- 15_动态规划/30_LC139单词拆分.cpp
+++ 15_动态规划/30_LC139单词拆分.cpp
@@ -1,20 +1,41 @@
 class Solution {
-   public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> st;
-        for (string& str : wordDict) {
-            st.insert(str);
+public:
+  bool wordBreak(string s, vector<string> &wordDict) {
+    unordered_set<string> st(wordDict.begin(), wordDict.end());
+    int n = s.length();
+    vector<bool> dp(n + 1, false);
+    dp[0] = true;
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 0; j < i; ++j) {
+        if (dp[j] && st.count(s.substr(j, i - j))) {
+          dp[i] = true;
+          break;
         }
-        vector<bool> dp(s.length() + 1, false);
-        dp[0] = true;
-        for (int i = 1; i <= s.length(); ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (dp[j] && st.find(s.substr(j, i - j)) != st.end()) {
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-        return dp[s.length()];
+      }
     }
+    return dp[n];
+  }
+};
+
+// 区间dp解法，较慢，好理解
+class Solution {
+public:
+  bool wordBreak(string s, vector<string> &wordDict) {
+    unordered_set<string> st(wordDict.begin(), wordDict.end());
+    int n = s.length();
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    for (int len = 1; len <= n; ++len) {
+      for (int l = 0; l + len - 1 < n; ++l) {
+        int r = l + len - 1;
+        if (st.count(s.substr(l, len))) {
+          dp[l][r] = true;
+        } else {
+          for (int p = l; p < r; ++p) {
+            dp[l][r] = dp[l][r] || (dp[l][p] && dp[p + 1][r]);
+          }
+        }
+      }
+    }
+    return dp[0][n - 1];
+  }
 };
