6daf8d75c04a857a4db71ba33f0d1f95e99c75c1
Sugar <45682h@gmail.com>
2023-09-24 21:33:56 +0800

    [feat] 优化写法

--- 15_动态规划/32_LC647回文子串.cpp
+++ 15_动态规划/32_LC647回文子串.cpp
@@ -1,42 +1,45 @@
 class Solution {
-   public:
-    int countSubstrings(string s) {
-        int n = s.length();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int ans = 0;
-        for (int j = 0; j < n; ++j) {
-            for (int i = 0; i <= j; ++i) {
-                if (s[i] == s[j] && (j - i <= 2 || dp[i + 1][j - 1])) {
-                    dp[i][j] = true;
-                    ++ans;
-                }
-            }
+public:
+  int countSubstrings(string s) {
+    int n = s.length();
+    int ans = n;
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    for (int i = 0; i < n; ++i)
+      dp[i][i] = true;
+    for (int i = n - 1; i >= 0; --i) {
+      for (int j = i + 1; j < n; ++j) {
+        if (s[i] == s[j]) {
+          dp[i][j] = (j - i + 1 <= 3) || dp[i + 1][j - 1];
+          if (dp[i][j])
+            ++ans;
         }
-        return ans;
+      }
     }
+    return ans;
+  }
 };
 
 // 中心拓展法，更快
 class Solution {
-  public:
-    int countSubstrings(string s) {
-        int n = s.length();
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            ++ans;
-            int l = i - 1, r = i + 1;
-            while (l >= 0 && r < n && s[l] == s[r]) {
-                ++ans;
-                --l, ++r;
-            }
-        }
-        for (int i = 0; i < n - 1; ++i) {
-            int l = i, r = i + 1;
-            while (l >= 0 && r < n && s[l] == s[r]) {
-                ++ans;
-                --l, ++r;
-            }
-        }
-        return ans;
+public:
+  int countSubstrings(string s) {
+    int n = s.length();
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      ++ans;
+      int l = i - 1, r = i + 1;
+      while (l >= 0 && r < n && s[l] == s[r]) {
+        ++ans;
+        --l, ++r;
+      }
     }
+    for (int i = 0; i < n - 1; ++i) {
+      int l = i, r = i + 1;
+      while (l >= 0 && r < n && s[l] == s[r]) {
+        ++ans;
+        --l, ++r;
+      }
+    }
+    return ans;
+  }
 };
