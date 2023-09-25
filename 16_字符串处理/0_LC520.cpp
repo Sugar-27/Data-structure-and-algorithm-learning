adeb77f6079222abda930772bf978ad4d3338359
Sugar <45682h@gmail.com>
2023-09-25 23:19:22 +0800

    [feat] format

--- 16_字符串处理/0_LC520.cpp
+++ 16_字符串处理/0_LC520.cpp
@@ -2,12 +2,44 @@ class Solution {
 public:
     bool detectCapitalUse(string word) {
         int n = word.length();
-        if (n >= 2 && islower(word[0]) && isupper(word[1]))    return false;
-        else if (n <= 2)    return true;
+        if (n >= 2 && islower(word[0]) && isupper(word[1]))
+            return false;
+        else if (n <= 2)
+            return true;
         for (int i = 2; i < n; ++i) {
-            if ((islower(word[i]) && islower(word[i - 1])) || (isupper(word[i]) && isupper(word[i - 1]))) continue;
+            if ((islower(word[i]) && islower(word[i - 1])) || (isupper(word[i]) && isupper(word[i - 1])))
+                continue;
             else {
                 return false;
+            }
+        }
+        return true;
+    }
+};
+
+// 规则判断，对于每一个单词
+// 1. 遇到小写字母的时候它前面最多一个大写字母
+// 2. 遇到大写字母的时候它前面不应该出现小写字母
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        bool ck = false;
+        int idx = 0;
+        int n = word.size();
+        while (idx < n) {
+            bool ck = false;
+            int cnt = 0;
+            while (idx < n && word[idx] != ' ') {
+                if (word[idx] >= 'a' && word[idx] <= 'z') {
+                    ck = true;
+                    if (cnt > 1)
+                        return false;
+                } else if (word[idx] >= 'A' && word[idx] <= 'Z') {
+                    if (ck)
+                        return false;
+                    ++cnt;
+                }
+                ++idx;
             }
         }
         return true;
--- 16_字符串处理/1_LC859.cpp
+++ 16_字符串处理/1_LC859.cpp
@@ -1,18 +1,19 @@
 // 分两种情况讨论
 // 1. s 与 goal相同，如果s中存在重复元素，则交换重复元素可构成亲密字符，返回true，否则返回false;
-// 2. s 与 goal不相同，如果长度不相等，直接返回false; 长度相等则记录s中不同元素的坐标，根据题意，
-// s 与 goal不同的元素只可以存在两个，因此如果记录的不同元素坐标数量大于2或小于2，返回false;
+// 2. s 与 goal不相同，如果长度不相等，直接返回false;
+// 长度相等则记录s中不同元素的坐标，根据题意，s与goal不同的元素只可以存在两个，因此如果记录的不同元素坐标数量大于2或小于2，返回false;
 // 如果等于2，判断交换后的s是否与goal相等，相等返回true否则返回false。
-
 
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
-        if (s.length() != goal.length())    return false;
+        if (s.length() != goal.length())
+            return false;
         if (s == goal) {
             unordered_map<int, int> mp;
             for (char ch : s) {
-                if (mp[ch] == 1)    return true;
+                if (mp[ch] == 1)
+                    return true;
                 mp[ch]++;
             }
             return false;
@@ -20,11 +21,13 @@ public:
             vector<int> diff;
             for (int i = 0; i < s.length(); ++i) {
                 if (s[i] != goal[i]) {
-                    if (diff.size() == 2)   return false;
+                    if (diff.size() == 2)
+                        return false;
                     diff.push_back(i);
                 }
             }
-            if (diff.size() == 1)   return false;
+            if (diff.size() == 1)
+                return false;
             swap(s[diff[0]], s[diff[1]]);
             return s == goal;
         }
